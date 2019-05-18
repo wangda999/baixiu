4d8637d07d738a3ebd4b84cd0bb27e4972a6635f
zxk <276442562@qq.com>
2019-05-18 11:48:33 +0800

    Setp-03 侧边栏高亮

--- admin/inc/aside.php
+++ admin/inc/aside.php
@@ -1,36 +1,45 @@
+<?php
+
+$path = $_SERVER['PHP_SELF'];
+$path = substr($path, 7);
+
+ ?>
 <div class="aside">
     <div class="profile">
       <img class="avatar" src="/static/uploads/avatar.jpg">
       <h3 class="name">布头儿</h3>
     </div>
     <ul class="nav">
-      <li class="active">
-        <a href="index.html"><i class="fa fa-dashboard"></i>仪表盘</a>
+      <li <?php echo $path === 'index.php'? 'class="active"' : ''; ?>>
+        <a href="/admin/index.php"><i class="fa fa-dashboard"></i>仪表盘</a>
       </li>
-      <li>
-        <a href="#menu-posts" class="collapsed" data-toggle="collapse">
+      <?php $menu_posts = array('posts.php', 'post-add.php', 'categories.php')?>
+      <li <?php echo in_array($path, $menu_posts) ? 'class="active"' : ''; ?>>
+
+        <a href="#menu-posts" <?php echo in_array($path, $menu_posts)? '' : 'class="collapsed"'; ?> data-toggle="collapse">
           <i class="fa fa-thumb-tack"></i>文章<i class="fa fa-angle-right"></i>
         </a>
-        <ul id="menu-posts" class="collapse">
-          <li><a href="posts.html">所有文章</a></li>
-          <li><a href="post-add.html">写文章</a></li>
-          <li><a href="categories.html">分类目录</a></li>
+        <ul id="menu-posts" class="collapse<?php echo in_array($path, $menu_posts)? ' in' : ''; ?>">
+          <li <?php echo $path === 'posts.php'? 'class="active"' : ''; ?>><a href="/admin/posts.php" >所有文章</a></li>
+          <li <?php echo $path === 'post-add.php'? 'class="active"' : ''; ?>><a href="/admin/post-add.php" >写文章</a></li>
+          <li <?php echo $path === 'categories.php'? 'class="active"' : ''; ?>><a href="/admin/categories.php" >分类目录</a></li>
         </ul>
       </li>
-      <li>
-        <a href="comments.html"><i class="fa fa-comments"></i>评论</a>
+      <li <?php echo $path === 'comments.php'? 'class="active"' : ''; ?>>
+        <a href="/admin/comments.php"><i class="fa fa-comments"></i>评论</a>
       </li>
-      <li>
-        <a href="users.html"><i class="fa fa-users"></i>用户</a>
+      <li <?php echo $path === 'users.php'? 'class="active"' : ''; ?>>
+        <a href="/admin/users.php"><i class="fa fa-users"></i>用户</a>
       </li>
-      <li>
-        <a href="#menu-settings" class="collapsed" data-toggle="collapse">
+      <?php   $menu_settings =array('nav-menus.php', 'slides.php', 'settings.php'); ?>
+      <li <?php echo in_array($path, $menu_settings) ? 'class="active"' : ''; ?>>
+        <a href="/admin/#menu-settings" <?php echo in_array($path, $menu_settings) ? '' : 'class="collapsed"'; ?> data-toggle="collapse">
           <i class="fa fa-cogs"></i>设置<i class="fa fa-angle-right"></i>
         </a>
-        <ul id="menu-settings" class="collapse">
-          <li><a href="nav-menus.html">导航菜单</a></li>
-          <li><a href="slides.html">图片轮播</a></li>
-          <li><a href="settings.html">网站设置</a></li>
+        <ul id="menu-settings" class="collapse<?php echo in_array($path, $menu_settings)? ' in' : ''; ?>">
+          <li<?php echo $path === 'nav-menus.php'? 'class="active"' : ''; ?>><a href="/admin/nav-menus.php">导航菜单</a></li>
+          <li<?php echo $path === 'slides.php'? 'class="active"' : ''; ?>><a href="/admin/slides.php">图片轮播</a></li>
+          <li<?php echo $path === 'settings.php'? 'class="active"' : ''; ?>><a href="/admin/settings.php">网站设置</a></li>
         </ul>
       </li>
     </ul>
