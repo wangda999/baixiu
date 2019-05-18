fa70b278d737fee8d193e74bd2ca642e6b25e3cc
zxk <276442562@qq.com>
2019-05-18 10:45:49 +0800

    Step-02 抽取公共样式 设置配置信息

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * 百秀博客项目中的配置文件
+ */
+
+/**
+ * 数据库主机
+ */
+define('ZXK_DB_HOST', '127.0.0.1');
+/**
+ * 数据库账号
+ */
+define('ZXK_DB_USER', 'root');
+/**
+ * 数据库密码
+ */
+define('ZXK_DB_PASS', '123456');
+/**
+ * 数据库名字
+ */
+define('ZXK_DB_NAME', 'baixiu');
